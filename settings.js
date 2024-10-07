fd38ab2bd2aded6b90f61b1e1c1bc71aacdd3429
JusNero <166287539+JusNero@users.noreply.github.com>
2024-10-07 10:53:02 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2347063956321"
-global.ownername = "Precious Ayomide👑"
+global.ownernomer = "2347071518663"
+global.ownername = "Queen priya👑"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
